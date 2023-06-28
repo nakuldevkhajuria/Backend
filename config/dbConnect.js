0e14f206af48324bfbaa0f8df42bf5b098ae9d7b
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-06-29 00:04:56 +0530

    config and model for user and its conenction towards db

---
+++ config/dbConnect.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const dbConnect =async()=>{
+    try {
+       await mongoose.connect(process.env.MONGO_URL,
+        {
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        }
+        )   
+        console.log('db is conencted') 
+    } 
+    catch (error) {
+        throw error;
+    }
+}
+
+module.exports = dbConnect;
---
+++ models/UserModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+
+const UserSchema = new mongoose.Schema(
+    {
+        email:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        password:{
+            type:String,
+            required:true,
+        }
+    }
+)
+
+UserSchema.pre("save",async(next)=>{
+    let salt = 10;
+     this.password =  await bcrypt.hash(this.password,salt)
+    next();
+})
+
+UserSchema.methods.isPasswordCorrect = async function(enteredPassword){
+return bcrypt.compare(enteredPassword,this.password)
+}
+
+module.exports = mongoose.model("users",UserSchema)
