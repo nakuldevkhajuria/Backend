1ef591f4e47cd19169116031920a4963fae4f93d
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-07-01 17:56:10 +0530

    come editing is done

--- controllers/CreateRoom.js
+++ controllers/CreateRoom.js
@@ -1,21 +1,22 @@
-const Room = require('../models/Room');
+
+const RoomModel = require('../models/RoomModel');
 
 // @route   POST /room/join/:roomId
 // @desc    Add user to a room
 exports.joinRoom = async (req, res, next) => {
-  const { user } = req;
+  const { userData } = req.userData;
   const { roomId } = req.params;
 
   try {
-    let room = await Room.findById(roomId);
+    let room = await RoomModel.findById(roomId);
     // Check if user already in room
     const userInRoom = room.users.find((roomUser) => {
-      return roomUser._id == user.id ? true : false;
+      return roomUser._id == userData.id ? true : false;
     });
     if (userInRoom) {
       return res.status(400).json({ errors: [{ msg: 'Already joined' }] });
     }
-    room.users.push(user.id);
+    room.users.push(userData.id);
     room.populate('users', { password: 0 });
     room = await room.save();
     res.status(200).json({ msg: 'Successfully joined', room });
